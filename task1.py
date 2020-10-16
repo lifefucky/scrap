c7d117bc10c655afd9a1f1d94adb90675337b75e
lifefucky <ip.udachin@gmail.com>
2020-10-16 18:59:36 +0300

    task1 completed

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
-print('Hello')
+import requests
+from getpass import getpass
+
+
+login='lifefucky'
+password=input('Press the password:')
+
+r = requests.get('https://api.github.com/user/repos', auth=(login, password))
+for repo in r.json():
+    if not repo['private']:
+        print(repo['name'])
