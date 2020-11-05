ca6b9078d78fe5701e9afcaf69f2131298c197de
lifefucky <ip.udachin@gmail.com>
2020-11-05 11:25:25 +0300

    inserted task texts

--- task2.py
+++ task2.py
@@ -1,18 +1,7 @@
-
-'''2. Написать функцию, которая производит поиск и выводит на экран вакансии с заработной платой больше
-введённой суммы. Запрос должен анализировать одновременно минимальную и максимальную зарплату.'''
-
-from pymongo import MongoClient
-
-client = MongoClient('127.0.0.1', 27017)
-db = client['jobs']
-collection = db['jobs_list']
-
-wish_salary=int(input('please insert your wishing salary:'))
-
-for i in collection.find({'$or':[{'SalaryFrom':{'$gte':wish_salary}},
-                       {'$and':[{'SalaryFrom':{'$eq':None}}, {'SalaryTo':{'$gte':wish_salary}}]}
-                                ]
-                          }):
-    print(i)
-
+'''Написать приложение, которое собирает основные новости с сайтов news.mail.ru, lenta.ru, yandex-новости.
+Для парсинга использовать XPath. Структура данных должна содержать:
+название источника;
+наименование новости;
+ссылку на новость;
+дата публикации.
+Сложить собранные данные в БД'''
